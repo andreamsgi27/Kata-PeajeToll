0d91919a4dafa34b919ab949d3f6675299bedb55
andreamsgi27 <andreamsgi27@hotmail.com>
2024-11-27 12:00:31 +0100

    estructura test

--- demo/src/test/java/com/example/demo/TollStationTest.java
+++ demo/src/test/java/com/example/demo/TollStationTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
-
-	@Test
+public class TollStationTest {
+    
+    @Test
 	void contextLoads() {
 	}
-
+    
 }
